5721ce3682bb907ee4b4ae8e5d394d7fe73927d9
Eric Ihli <eihli@owoga.com>
2024-03-29 17:57:25 -0700

    update __main__ to use example of new dataloader

--- gato/tasks/vqa_task.py
+++ gato/tasks/vqa_task.py
@@ -1,3 +1,4 @@
+from typing import Any
 # Assume all datasets are downloaded and available from local directories
 from gato.tasks.task import Task
 
@@ -15,12 +16,10 @@ import random
 from torch.utils.data import DataLoader
 from tokenizers import Tokenizer
 from transformers import AutoTokenizer, GPT2Tokenizer
+import datasets
 
 class VqaTask(Task): 
     def __init__(self, train_dataloader: DataLoader, test_dataloader: DataLoader):
-        """
-        vqa_dataset is the directory where the data for vqa task is located, should end with "/" 
-        """
         super().__init__()
         self.train_dataloader = train_dataloader
         self.train_dataloader_iterator = iter(train_dataloader)
@@ -73,26 +72,42 @@ class VqaTask(Task):
         }
         return metrics
 
+class IdentityTransform():
+    def __call__(self, sample: Any):
+        return sample
+
+class VqaDataset(datasets.IterableDataset):
+    def __init__(self, dataset: datasets.Dataset, transform=IdentityTransform()):
+        self.transform = transform
+        self.dataset = dataset.to_iterable_dataset(num_shards=8).shuffle(seed=42)
+
+    def __iter__(self):
+        for sample in iter(self.dataset):
+            answers = sample['answers']
+            del sample['answers']
+            sample.update(random.choice(answers))
+            yield self.transform(sample)
 
 # test code
 if __name__ == '__main__':
+    from torchvision.transforms import v2 as transforms
     # replace the following directories and files names with your directories and files
-    task = VqaTask(vqa_dataset              = '/home/<user name>/Git/NEKO/VQA_Data/',
-                   train_data               = ['train2014'], 
-                   test_data                = ['val2014'],
-                   train_img_name_prefix    = ['COCO_train2014_'], 
-                   train_img_file_name_len  = [27], 
-                   test_img_name_prefix     = ['COCO_val2014_'], 
-                   test_img_file_name_len   = [25],
-                   questions_file           = 'questions.json', 
-                   annotations_file         = 'annotations.json'
-                   )
+    ok_vqa_builder = datasets.load_dataset_builder('HuggingFaceM4/OK-VQA', trust_remote_code=False)
+    ok_vqa_builder.download_and_prepare(file_format='arrow')
+    ds = ok_vqa_builder.as_dataset()
+    vqa_transforms = transforms.Compose([
+        transforms.PILToTensor(),
+        transforms.CenterCrop(1024),
+        transforms.Resize(size=(256, 256))
+    ])
+    task = VqaTask(
+        DataLoader(VqaDataset(ds['train'], vqa_transforms), batch_size=4),
+        DataLoader(VqaDataset(ds['validation'], vqa_transforms), batch_size=4),
+    )
 
-    batch = task.sample_batch(5)
+    batch = task.sample_batch()
     print(type(batch))
-    print(list(batch[0].keys()))
-    print(batch[0]['images'][0][1][10])
-    print(batch[0]['images'][0][2][15])
-    print(batch[0]['images'].shape)
-    print(batch[0]['text'])
-
+    print(list(batch.keys()))
+    print(f'image shape: {batch["image"].shape}')
+    print(batch['question'])
+    print(batch['answer'])
